071c304f15978ba18c4c653fe713e40fe1dd82f4
André Freitas <freitasandre38@gmail.com>
2023-04-23 20:44:10 -0300

    feat: initial steps

---
+++ api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	fmt.Println("Rodando aplicação")
+	app.Listen(":3000")
+}
---
+++ api/src/router.go
@@ -0,0 +1,13 @@
+package src
+
+// func handler(c *fiber.Ctx) error {
+// 	result := c.Params("foo")
+
+// 	buffer := make([]byte, len(result))
+// 	copy(buffer, result)
+// 	resultCopy := string(buffer)
+// }
+
+// app.Get("/:foo", func (c *fiber.Ctx) error {
+// 	result := utils.CopyString(c.Params("foo"))
+// })
