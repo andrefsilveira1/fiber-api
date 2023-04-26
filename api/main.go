069b99e12fe0fd0c5c38769e0dbc351fa2c65b0e
André Freitas <freitasandre38@gmail.com>
2023-04-25 21:54:28 -0300

    feat: implementing new post routes

--- api/main.go
+++ api/main.go
@@ -2,34 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
 )
 
+type Person struct {
+	Name string `json:"name" form:"name"`
+	Pass string `json:"pass" form:"pass"`
+}
+
 func main() {
-	engine := html.New("./public", ".html")
+	engine := html.New("./public/views/", ".html")
 	app := fiber.New(fiber.Config{
-		Views: engine,
+		Views:       engine,
+		ViewsLayout: "../partials/template",
 	})
 	app.Static("/", "../public")
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{
+	app.Get("/home", func(c *fiber.Ctx) error {
+		return c.Render("home/index", fiber.Map{
 			"Title":    "Portfólio",
 			"Subtitle": "This portfolio was developed by André Freitas Silveira",
 		})
 	})
 
-	app.Get("/:value", func(c *fiber.Ctx) error {
+	app.Get("value/:value", func(c *fiber.Ctx) error {
 		return c.SendString("Value: " + c.Params("value"))
-		//Getting request from the parameters
+
 	})
 
-	app.Get("/api/*", func(c *fiber.Ctx) error {
-		return c.SendString("Path: " + c.Params("*"))
-		//Getting request from the parameters
+	app.Get("api/", func(c *fiber.Ctx) error {
+		return c.Render("api/index", fiber.Map{})
+	})
+
+	app.Post("api/enviar", func(c *fiber.Ctx) error {
+		person := new(Person)
+		if err := c.BodyParser(person); err != nil {
+			return err
+		}
+		log.Println(person.Name)
+		log.Println(person.Pass)
+
+		return c.Render("api", fiber.Map{
+			"Nome": person.Name,
+			"Pass": person.Pass,
+		})
 	})
 
 	fmt.Println("Rodando aplicação")
 	app.Listen(":3000")
+
 }
