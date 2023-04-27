a9c41dcc404eb1d11bd751d877b9664108baea0e
André Freitas <freitasandre38@gmail.com>
2023-04-26 21:33:13 -0300

    feat: implementing new conditions

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/mail"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
@@ -14,14 +15,13 @@ type Person struct {
 }
 
 func main() {
-	engine := html.New("./public/views/", ".html")
+	engine := html.New("./src/public/views", ".html")
 	app := fiber.New(fiber.Config{
-		Views:       engine,
-		ViewsLayout: "../partials/template",
+		Views: engine,
 	})
-	app.Static("/", "../public")
+	app.Static("/", "./public")
 	app.Get("/home", func(c *fiber.Ctx) error {
-		return c.Render("home/index", fiber.Map{
+		return c.Render("index", fiber.Map{
 			"Title":    "Portfólio",
 			"Subtitle": "This portfolio was developed by André Freitas Silveira",
 		})
@@ -31,9 +31,8 @@ func main() {
 		return c.SendString("Value: " + c.Params("value"))
 
 	})
-
 	app.Get("api/", func(c *fiber.Ctx) error {
-		return c.Render("api/index", fiber.Map{})
+		return c.Render("send", fiber.Map{})
 	})
 
 	app.Post("api/enviar", func(c *fiber.Ctx) error {
@@ -43,8 +42,21 @@ func main() {
 		}
 		log.Println(person.Name)
 		log.Println(person.Pass)
+		_, err := mail.ParseAddress(person.Name)
+		if err != nil {
+			condition := true
+			log.Println("Erro: Email inválido!", err)
+			return c.Render("send", fiber.Map{
+				"Erro":     err,
+				"hasError": condition,
+			})
+		}
 
-		return c.Render("api", fiber.Map{
+		output := make([]Person, 1)
+		output = append(output, *person)
+		log.Println(output)
+
+		return c.Render("send", fiber.Map{
 			"Nome": person.Name,
 			"Pass": person.Pass,
 		})
